37335181480742cf9a0d0127fae15632054ba730
Anthony Sottile <asottile@umich.edu>
2014-04-18 17:13:32 -0700

    Update setup metadata

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 
 setup(
     name='yelp_bytes',
+    description='Utilities for dealing with byte strings, invented and maintained by Yelp.',
+    url='https://github.com/Yelp/yelp_bytes',
     version='0.1.0',
-    description="Byte utilities maintained by Yelp",
+
     author='Buck Golemon',
+    author_email='buck@yelp.com',
+
     platforms='all',
     classifiers=[
         'License :: Public Domain',
