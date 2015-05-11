09d8a89f36e17f76ade388ce4ca6a922ab7d59b1
Anthony Sottile <asottile@umich.edu>
2015-05-11 16:00:30 -0700

    v0.2.0

--- setup.py
+++ setup.py
@@ -4,16 +4,22 @@ setup(
     name='yelp_bytes',
     description='Utilities for dealing with byte strings, invented and maintained by Yelp.',
     url='https://github.com/Yelp/yelp_bytes',
-    version='0.1.1',
+    version='0.2.0',
 
-    author='Buck Golemon',
+    author='Buck Evan',
     author_email='buck@yelp.com',
 
     platforms='all',
     classifiers=[
         'License :: Public Domain',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=['yelp_encodings'],
 
