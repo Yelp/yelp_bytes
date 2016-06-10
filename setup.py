5eafb14047d33daaf1afaa9a5ae3b7b8438565a4
Buck Evan <buck@yelp.com>
2016-06-10 11:02:48 -0700

    this is v0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='yelp_bytes',
     description='Utilities for dealing with byte strings, invented and maintained by Yelp.',
     url='https://github.com/Yelp/yelp_bytes',
-    version='0.2.0',
+    version='0.3.0',
 
     author='Buck Evan',
     author_email='buck@yelp.com',
@@ -13,15 +13,19 @@ setup(
     classifiers=[
         'License :: Public Domain',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=['yelp_encodings'],
 
     py_modules=['yelp_bytes'],
+    options={
+        'bdist_wheel': {
+            'universal': 1,
+        }
+    },
 )
